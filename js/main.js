ddcc86a8cef4d9a2972bf13574c0fb367fcd4e29
SHEMA Fred <shemafred3@gmail.com>
2021-07-21 18:45:10 +0200

    web-initial

---
+++ js/main.js
@@ -0,0 +1,37 @@
+$(function () {
+  $("form#contact-form").on("submit", function (e) {
+    e.preventDefault();
+    var names = $("#contact-form input#names").val();
+    var phone = $("#contact-form input#phone").val();
+    var email = $("#contact-form input#email").val();
+    var service = $("#contact-form select#service").val();
+    var message = $("#contact-form textarea#message").val();
+    const TID = "template_w4m9dd5";
+    const SID = "service_2xxp7ho";
+
+    const params = {
+      names,
+      email,
+      service,
+      phone,
+      message,
+    };
+    console.log(params);
+    $this = $("#submit-btn");
+    $this.prop("disabled", true);
+    $this.addClass("load");
+    // emailjs
+    //   .send(SID, TID, params)
+    //   .then(function (res) {
+    //     alert("Email sent successfully.");
+    //   })
+    //   .catch(function (err) {
+    //     console.log(err);
+    //     alert("Failed to send message");
+    //   })
+    //   .finally(() => {
+    //     $this.prop("disabled", false);
+    //     $this.removeClass("load");
+    //   });
+  });
+});
